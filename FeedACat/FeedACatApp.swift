cad43768e1aab2346c3f4b94fb44026be4ebf746
Oleksii Mosunov <o.mosunov@mobidev.biz>
2022-12-12 20:14:10 +0200

    Initial Commit

---
+++ FeedACat/FeedACatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedACatApp.swift
+//  FeedACat
+//
+//  Created by Alex Mosunov on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FeedACatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
